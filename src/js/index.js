77c9f1bd9db2d4aaf764e9920874e8144394a8d5
Konrad <konradhbr@gmail.com>
2020-09-21 18:01:11 +0200

    Finish responsivity web design

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,45 @@
 // logo slider -------------------------------------------
+// eslint-disable-next-line no-undef
 $(document).ready(() => {
+  // eslint-disable-next-line no-undef
   $('.wedding-logo ').slick({
-    slidesToShow: 6,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1500,
+    autoplaySpeed: 2000,
+    speed: 700,
     arrows: false,
     dots: false,
     pauseOnHover: false,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
-      },
-    }, {
-      breakpoint: 520,
-      settings: {
-        slidesToShow: 2,
-      },
-    }],
+    responsive: [
+      {
+        breakpoint: 2160,
+        settings: {
+          slidesToShow: 3,
+        },
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+        },
+      }],
   });
 });
 
-// hamburger click-------------------------------------------------
+// hamburger and dropdown click-------------------------------------------------
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
+const navbar = document.querySelector('#services');
 
 const hamburgerHandleClick = () => {
   hamburger.classList.toggle('hamburger--active');
   mobileMenu.classList.toggle('mobile-menu--active');
 };
+
+const navbarHandleClick = () => {
+  document.querySelector('.dropdown').style.display = 'block';
+};
+
 hamburger.addEventListener('click', hamburgerHandleClick);
+navbar.addEventListener('click', navbarHandleClick);
+
